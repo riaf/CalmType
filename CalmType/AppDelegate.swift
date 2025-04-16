981161e6f90b493a9cb87dddd667ab623fc4eeae
Keisuke SATO <riaf@me.com>
2025-04-16 16:44:52 +0900

    Show main window launched

--- CalmType/AppDelegate.swift
+++ CalmType/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         setupKeyboardShortcuts()
         setupNotificationObserver()
         setupLocalEventMonitor()
+        showMainWindow()
     }
 
     func applicationWillTerminate(_ notification: Notification) {
